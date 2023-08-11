abeea019a008d299ac1d7114cd85978570ce176e
yasar <ysar0ysamaz@gmail.com>
2023-08-11 22:30:59 +0300

    commit02

--- src/main/java/AtmClass.java
+++ src/main/java/AtmClass.java
@@ -86,6 +86,9 @@ public class AtmClass {
             case 4:
                 cikis();
                 break;
+            default:
+                System.out.println("yanlis bir deger girdiniz");
+                devam();
         }
     }
 
